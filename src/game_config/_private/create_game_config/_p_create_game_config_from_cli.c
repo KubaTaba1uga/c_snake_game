1560d46b522a0d2fef1958e06fcf1257d08612ad
KubaTaba1uga <jakub@buczynski.net.pl>
2023-10-29 09:58:30 +0100

    add cli parsing

---
+++ src/game_config/_private/create_game_config/_p_create_game_config_from_cli.c
@@ -0,0 +1,54 @@
+#include "_p_create_game_config_from_cli.h"
+#include "../_p_game_config.h"
+#include "game_config.h"
+#include <stddef.h>
+
+inline game_config_ptr create_game_type(int argc, char *argv[],
+                                        game_config_ptr game_conifg);
+inline game_config_ptr create_difficulty(int argc, char *argv[],
+                                         game_config_ptr game_conifg);
+inline game_config_ptr create_size(int argc, char *argv[],
+                                   game_config_ptr game_conifg);
+
+game_config_ptr create_game_config_from_cli(int argc, char *argv[]) {
+  game_config_ptr (*const creation_functions[])(int argc, char *argv[],
+                                                game_config_ptr game_conifg) = {
+      create_game_type, create_difficulty, create_size};
+  game_config_ptr game_config;
+  size_t i;
+
+  game_config = create_game_config();
+
+  if (!game_config) {
+    // TO-DO set errno to OOM
+    // TO-DO log messgae
+    return NULL;
+  }
+
+  for (i = 0; i < sizeof(creation_functions) / sizeof(void *); i++) {
+    game_config = creation_functions[i](argc, argv, game_config);
+
+    if (!game_config) {
+      // TO-DO set errno to CONFIG CREATION ERROR
+      // TO-DO log message
+      return NULL;
+    }
+  }
+
+  return game_config;
+}
+
+game_config_ptr create_game_type(int argc, char *argv[],
+                                 game_config_ptr game_conifg) {
+  return game_conifg;
+}
+
+game_config_ptr create_difficulty(int argc, char *argv[],
+                                  game_config_ptr game_conifg) {
+  return game_conifg;
+}
+
+game_config_ptr create_size(int argc, char *argv[],
+                            game_config_ptr game_conifg) {
+  return game_conifg;
+}
