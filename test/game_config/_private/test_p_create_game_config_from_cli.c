d2c114cef96854e3d0546b924eb7f3996361d160
KubaTaba1uga <jakub@buczynski.net.pl>
2023-10-29 10:45:53 +0100

    add test template

--- test/game_config/_private/test_p_create_game_config_from_cli.c
+++ test/game_config/_private/test_p_create_game_config_from_cli.c
@@ -1,3 +1,10 @@
+// Test framework
+#include <unity.h>
+
+// App
+#include "_p_create_game_config_from_cli.c"
+#include "_p_game_config.h"
+
 // Test utils
 #include "utils.h"
 
@@ -13,3 +20,18 @@ game_config_ptr game_config_mock = NULL;
 void setUp() { create_game_config_mock(); }
 
 void tearDown() { destroy_game_config_mock(); }
+
+/*******************************************************************************
+ *    TESTS
+ ******************************************************************************/
+
+void test_create_game_config_from_cli_success(void) {
+  int argc = 3;
+  char *argv[] = {"exec_path", "-a", "aslkdjakljdlkasjdlk"};
+
+  game_config_ptr game_config;
+
+  game_config = create_game_config_from_cli(argc, argv);
+
+  TEST_ASSERT_NULL(game_config);
+}
