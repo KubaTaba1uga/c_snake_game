fb4848a5ca134f7cfa2910e2158e0abebbbfc621
KubaTaba1uga <jakub@buczynski.net.pl>
2023-11-22 08:49:05 +0100

    fix build

--- src/controller/_private/local/stream_proxy.c
+++ src/controller/_private/local/stream_proxy.c
@@ -5,20 +5,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <c_lists/ar_list.h>
-#include <c_lists/cll_error.h>
+#include <chr_list.h>
 
 #include "../../../interfaces/std_lib_interface.h"
 #include "../../../proj_config/error.h"
 #include "stream_proxy.h"
 
-static arl_ptr create_stream_data(void);
+static chr_ptr create_stream_data(void);
 static stream_proxy_ptr reset_stream_data(stream_proxy_ptr stream_proxy);
 static void destroy_stream_data(stream_proxy_ptr stream_proxy);
 
 struct stream_proxy {
   FILE *stream;
-  arl_ptr data;
+  chr_ptr data;
   bool not_read;
 };
 
@@ -45,23 +44,32 @@ void destroy_stream_proxy(stream_proxy_ptr stream_proxy) {
 }
 
 stream_proxy_ptr flush_stream_proxy(stream_proxy_ptr stream_proxy) {
-  void *err;
+  void *recived;
+  char c;
 
   errno = 0;
 
-  err = reset_stream_data(stream_proxy);
+  recived = reset_stream_data(stream_proxy);
 
-  if (err) {
+  if (!recived) {
     // TO-DO log message
     goto ERROR;
   }
+
+  while ((c = fgetc(stream_proxy->stream)) != EOF) {
+    chr_append(stream_proxy->data, c);
+  }
+
+  chr_append(stream_proxy->data, 0);
+
+  return stream_proxy;
 
 ERROR:
   return NULL;
 }
 
 stream_proxy_ptr reset_stream_data(stream_proxy_ptr stream_proxy) {
-  arl_ptr list;
+  chr_ptr list;
 
   destroy_stream_data(stream_proxy);
 
@@ -71,6 +79,7 @@ stream_proxy_ptr reset_stream_data(stream_proxy_ptr stream_proxy) {
     goto ERROR;
 
   stream_proxy->data = list;
+  stream_proxy->not_read = true;
 
   return stream_proxy;
 
@@ -80,21 +89,21 @@ ERROR:
 
 void destroy_stream_data(stream_proxy_ptr stream_proxy) {
   if (stream_proxy->data)
-    arl_destroy(stream_proxy->data);
+    chr_destroy(stream_proxy->data);
 
   stream_proxy->data = NULL;
 }
 
-arl_ptr create_stream_data(void) {
+chr_ptr create_stream_data(void) {
   const size_t DEFAULT_SIZE = 255;
-  arl_ptr list;
-  cll_error_t err;
+  chr_ptr list;
+  chr_error err;
 
-  err = arl_create(&list, DEFAULT_SIZE);
+  err = chr_create(&list, DEFAULT_SIZE);
 
   if (err) {
     switch (err) {
-    case CLL_ERROR_OUT_OF_MEMORY:
+    case CHR_ERROR_OUT_OF_MEMORY:
       errno = ERROR_OOM;
       goto ERROR;
     default:
--- test/test_controller.d/test_stream_proxy.c
+++ test/test_controller.d/test_stream_proxy.c
@@ -2,28 +2,66 @@
 #include "controller/_private/local/stream_proxy.c"
 #include "mock_std_lib_interface.h"
 #include "unity.h"
+#include <chr_list.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <unistd.h>
 
+const char user_input[] = {'W', 'W', 'W', 'W', 'W', 'S',
+                           'A', 'D', 'S', 'S', 'S'};
 stream_proxy_ptr stream_proxy_mock;
 FILE *tmp_file;
 
 void setUp() {
+  size_t i;
+
   stream_proxy_mock = create_stream_proxy_mock();
 
-  tmp_file = fopen("tmp.file", "w+");
+  tmp_file = tmpfile();
   if (!tmp_file)
     TEST_FAIL_MESSAGE("Unable to open temporary file");
+
+  for (i = 0; i < sizeof(user_input) / sizeof(char); i++) {
+    fputc(user_input[i], tmp_file);
+  }
+  fseek(tmp_file, 0, SEEK_SET);
 }
 
 void tearDown() { destroy_stream_proxy_mock(stream_proxy_mock); }
 
-void test_flush_stream_data_success(void) {
+void test_create_stream_proxy_success(void) {
   stream_proxy_ptr stream_proxy;
 
   app_malloc_ExpectAndReturn(stream_proxy_expect_size, stream_proxy_mock);
 
   stream_proxy = create_stream_proxy(tmp_file);
 
-  flush_stream_proxy(stream_proxy);
+  TEST_ASSERT_EQUAL(stream_proxy->stream, tmp_file);
+  TEST_ASSERT_NULL(stream_proxy->data);
+  TEST_ASSERT_EQUAL(true, stream_proxy->not_read);
+}
+
+void test_flush_stream_data_success(void) {
+  stream_proxy_ptr stream_proxy;
+  char recived;
+  size_t i;
+
+  app_malloc_ExpectAndReturn(stream_proxy_expect_size, stream_proxy_mock);
+
+  stream_proxy = create_stream_proxy(tmp_file);
+
+  TEST_ASSERT_NULL(stream_proxy->data);
+
+  stream_proxy = flush_stream_proxy(stream_proxy);
+
+  TEST_ASSERT_NOT_NULL(stream_proxy);
+  TEST_ASSERT_NOT_NULL(stream_proxy->data);
+  TEST_ASSERT_EQUAL(sizeof(user_input) / sizeof(char) + 1,
+                    chr_length(stream_proxy->data));
+
+  for (i = 0; i < sizeof(user_input) / sizeof(char); i++) {
+    TEST_ASSERT_EQUAL(CHR_SUCCESS, chr_get(stream_proxy->data, i, &recived));
+    TEST_ASSERT_EQUAL(user_input[i], recived);
+  }
 }
