4a52dfbc58e284b93beb1f7a3e92baef99add7b9
KubaTaba1uga <jakub@buczynski.net.pl>
2023-11-23 09:01:11 +0100

    save progress

--- src/controller/_private/local/controller_local.c
+++ src/controller/_private/local/controller_local.c
@@ -1,10 +1,77 @@
 #include "../../../interfaces/std_lib_interface.h"
 #include "../../controller_type.h"
+#include "../../user_value.h"
 #include "../controller_private.h"
+#include "controller.h"
 #include "stream_proxy.h"
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct {
+  user_value_t user_value;
+  const char *string;
+} key_mapping;
+
+typedef struct {
+  stream_proxy_ptr stdin_proxy;
+  size_t i;
+} controller_local_private;
+
+static key_mapping keys_mappings[][4] = {
+    {
+        {.user_value = UP, .string = "w"},
+        {.user_value = DOWN, .string = "s"},
+        {.user_value = LEFT, .string = "a"},
+        {.user_value = RIGHT, .string = "d"},
+    },
+    {
+        {.user_value = UP, .string = "8"},
+        {.user_value = DOWN, .string = "5"},
+        {.user_value = LEFT, .string = "4"},
+        {.user_value = RIGHT, .string = "6"},
+    },
+};
+static stream_proxy_ptr get_stdin_proxy(void);
+
+controller_ptr init_controller_local(controller_ptr controller) {
+  static size_t counter = 0;
+
+  // Do not allow more controllers then there are configured key bindings.
+  if (counter >
+      sizeof(keys_mappings) / (sizeof(keys_mappings[0]) / sizeof(key_mapping)))
+    return NULL;
+
+  stream_proxy_ptr stdin_proxy = get_stdin_proxy();
+  if (!stdin_proxy)
+    return NULL;
+
+  controller_local_private private = {.i = counter, .stdin_proxy = stdin_proxy};
+
+  counter++;
+
+  set_controller_type(controller, CONTROLLER_LOCAL);
+  set_controller_private(controller, &private);
+
+  return controller;
+}
+
+void destroy_controller_local(controller_ptr controller) {
+  app_free(controller);
+  destroy_stream_proxy(get_stdin_proxy());
+}
+
+char *read_controller_local(controller_ptr controller) {
+  stream_proxy_ptr stream_proxy = get_controller_private(controller);
+  char buffer[get_length_proxy(stream_proxy)];
+  char *received;
+
+  received = read_stream_proxy(stream_proxy, buffer);
+  if (!received)
+    return NULL;
+}
+controller_ptr flush_controller_local(controller_ptr controller) {}
 
 stream_proxy_ptr get_stdin_proxy(void) {
   // Kindo singleton pattern
@@ -14,20 +81,4 @@ stream_proxy_ptr get_stdin_proxy(void) {
     return stream_proxy;
 
   return create_stream_proxy(stdin);
-}
-
-controller_ptr init_controller_local(controller_ptr controller) {
-  stream_proxy_ptr stdin_proxy = get_stdin_proxy();
-  if (!stdin_proxy)
-    return NULL;
-
-  set_controller_type(controller, CONTROLLER_LOCAL);
-  set_controller_private(controller, stdin_proxy);
-
-  return controller;
-}
-
-void destroy_controller_local(controller_ptr controller) {
-  app_free(controller);
-  destroy_stream_proxy(get_stdin_proxy());
 }
---
+++ src/controller/user_value.h
@@ -0,0 +1,20 @@
+#ifndef _user_value_h
+#define _user_value_h
+
+/*******************************************************************************
+ *   API
+ ******************************************************************************/
+typedef enum {
+  ENTER = 0,
+  ESCAPE,
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+
+  USER_VALUE_ENUM_LEN,
+} user_value_t;
+
+/* const char *str_controller_type(controller_type_t controller_type); */
+
+#endif
